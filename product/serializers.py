2f58e63a060376d8fc7a584807f1396a7367a025
Rajaul <uddin.rajaul1@gmail.com>
2024-03-11 22:53:33 +0545

    multiple queries

--- product/serializers.py
+++ product/serializers.py
@@ -5,22 +5,22 @@ from .models import Brand, Category, Product, ProductLine
 
 class CategorySerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source="name")
+
     class Meta:
         model = Category
         fields = ["category_name"]
 
+
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        exclude = ('id',)
-
+        exclude = ("id",)
 
 
 class ProductLineSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductLine
-        exclude = ('id',"is_active", "product")
-
+        exclude = ("id", "is_active", "product")
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -30,5 +30,11 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ("name", "slug", "description", "brand_name", "category_name", "product_line")
-
+        fields = (
+            "name",
+            "slug",
+            "description",
+            "brand_name",
+            "category_name",
+            "product_line",
+        )
--- product/views.py
+++ product/views.py
@@ -1,17 +1,19 @@
+from django.db import connection
 from django.shortcuts import render
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import Brand, Category, Product, ProductLine
+from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
 # Create your views here.
 
+
 class CategoryViewSet(viewsets.ViewSet):
     """
-        A simple Viewset for viewing all Categories
+    A simple Viewset for viewing all Categories
     """
 
     queryset = Category.objects.all()
@@ -23,43 +25,49 @@ class CategoryViewSet(viewsets.ViewSet):
 
 
 class BrandViewSet(viewsets.ViewSet):
-    """ 
-        Viewset for Brands
     """
+    Viewset for Brands
+    """
+
     queryset = Brand.objects.all()
+    print(connection.queries)
 
     @extend_schema(responses=BrandSerializer)
     def list(self, request):
         serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
+
 class ProductViewSet(viewsets.ViewSet):
-    """ 
-        A Viewset for Product
     """
+    A Viewset for Product
+    """
+
     queryset = Product.objects.all()
     lookup_field = "slug"
 
     def retrieve(self, request, slug=None):
-        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            many=True,
+        )
         return Response(serializer.data)
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
-    
+
     @action(
-            methods=["get"], 
-            detail=False, 
-            url_path=r"category/(?P<category>\w+)/all",
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
     )
     def list_product_by_category(self, request, category=None):
-        """ 
-            An endpoint to return product by category
         """
-        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
+        An endpoint to return product by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
-    
-    
-    
